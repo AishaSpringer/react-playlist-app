9baf0e49474734b505ba4937cf4d720dac45c50b
Aisha Springer <aishaspringer@gmail.com>
2022-10-13 18:11:39 -0400

    Render first components

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+            {/* <SearchBar /> */}
+            <div className="App-playlist">
+              {/* <SearchResults /> */}
+              {/* <Playlist /> */}
+            </div>
+        </div>
+      </div>
+    )
+  }
+}
 
 function App() {
   return (
