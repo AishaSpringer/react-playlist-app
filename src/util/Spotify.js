d89be0c972a3069aec0f96990726bd2e93da181f
Aisha Springer <aishaspringer@gmail.com>
2022-10-14 18:07:04 -0400

    set access token, expiration, and clear parameters

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -7,8 +7,16 @@ const Spotify = {
         }
 
         // check for access token match
-        const accessToken = window.location.href.match(/access_token=([^&]*)/);
+        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
         const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+        if (accessTokenMatch && expiresInMatch) {
+            accessToken = accessTokenMatch[1]; // set access token value
+            const expiresIn = Number(expiresInMatch[1]); // set variable for expiration time and set access token to expire at value for expiration time
+            window.setTimeout(() => accessToken = '', expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/');
+            return accessToken; // clears parameters from URL and allows us to grab new access token after previous one expires
+        }
     }
 }
 
