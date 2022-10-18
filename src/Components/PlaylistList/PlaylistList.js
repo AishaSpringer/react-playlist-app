92079dae132d8395e0e975ec4b1aae912f2e9e67
Aisha Springer <aishaspringer@gmail.com>
2022-10-18 12:54:33 -0400

    Initialize state for PlaylistList

--- src/Components/PlaylistList/PlaylistList.js
+++ src/Components/PlaylistList/PlaylistList.js
@@ -4,11 +4,14 @@ export class PlaylistList extends React.Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            playlists: []
+        };
     }
 
     render() {
         return(
-
+            // retrieve list of user's current playlists
         )
     }
 }
